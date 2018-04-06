23762fe5f2b0f8daff1e2a01ec567e4d88445b25
starlying <starlying@gmail.com>
2018-04-06 12:32:57 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,45 +6,75 @@ var rimraf = require("rimraf");
 var rename = require("gulp-rename");
 var replace = require("gulp-replace");
 var runSequence = require("run-sequence");
-var run = require('gulp-run');
+var child_process = require('child_process');
 
-var build = function(dir) {
-  return run('gitbook build ' + dir)
-    .exec()
-    .pipe(gulp.dest('output'));
+gulp.task("index-html", function () {
+  return gulp
+    .src('index.html')
+    .pipe(gulp.dest("./docs"));
+});
+
+gulp.task("index-assets", function () {
+  return gulp
+    .src(['assets/**/*'])
+    .pipe(gulp.dest("./docs/assets"));
+});
+
+var install = function (dir) {
+  child_process.execSync('gitbook install ' + dir, {stdio:[0,1,2]});
 }
 
-var copy = function(dir) {
-  return gulp
-    .src(["./getting-started/_book/**/*"])
-    .pipe(gulp.dest("./docs/getting-started"));
+var build = function (dir) {
+  child_process.execSync('gitbook build ' + dir, {stdio:[0,1,2]});
 }
 
-gulp.task("build-getting-started", function() {
-  return build('getting-started');
+var copy = function (dir) {
+  gulp
+    .src(["./" + dir + "/_book/**/*"])
+    .pipe(gulp.dest("./docs/" + dir + ""));
+  console.log(dir + ' build completed');
+}
+
+// gitbook main
+
+gulp.task("build-faq", function () {
+  install('faq');
+  build('faq');
+  copy('faq');
 });
 
-gulp.task("copy-getting-started", function() {
-  return copy('getting-started');
+gulp.task("build-getting-started", function () {
+  install('getting-started');
+  build('getting-started');
+  copy('getting-started');
 });
 
-gulp.task("index-html", function() {
-  return gulp
-  .src('index.html')
-  .pipe(gulp.dest("./docs"));
+gulp.task("build-introduction", function () {
+  install('introduction');
+  build('introduction');
+  copy('introduction');
 });
 
-gulp.task("index-assets", function() {
-  return gulp
-  .src(['assets/**/*'])
-  .pipe(gulp.dest("./docs/assets"));
+gulp.task("build-working-with-plugins", function () {
+  install('working-with-plugins');
+  build('working-with-plugins');
+  copy('working-with-plugins');
 });
 
-gulp.task("build", function(callback) {
+gulp.task("build-working-with-siteserver-cms", function () {
+  install('working-with-siteserver-cms');
+  build('working-with-siteserver-cms');
+  copy('working-with-siteserver-cms');
+});
+
+gulp.task("build", function (callback) {
   runSequence(
-    "build-getting-started",
-    "copy-getting-started",
     "index-assets",
-    "index-html"
+    "index-html",
+    "build-faq",
+    "build-getting-started",
+    "build-introduction",
+    "build-working-with-plugins",
+    "build-working-with-siteserver-cms"
   );
 });
